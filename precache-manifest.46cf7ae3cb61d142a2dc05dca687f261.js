64995e48a4f3a7dfa051133be68b2b2cad5df1fa
DmitryScaletta <dmitryscaletta@mail.ru>
2019-05-01 02:01:56 +0300

    Updates

---
+++ precache-manifest.46cf7ae3cb61d142a2dc05dca687f261.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcfa4ecbc1ce31638bdba578aa41769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51e13b9d57b3dd169320",
+    "url": "/static/js/2.8b8921a1.chunk.js"
+  },
+  {
+    "revision": "95b091b664f46f64117f",
+    "url": "/static/js/main.04c3d32e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
