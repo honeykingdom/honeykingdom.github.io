bd28f4df6fab6a54434cd954695533fb7159af49
DmitryScaletta <dmitryscaletta@mail.ru>
2019-05-03 15:16:54 +0300

    feat: use fullscreen polyfills

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import fullscreenPolyfills from './utils/fullscreen';
+
+fullscreenPolyfills();
 
 ReactDOM.render(<App />, document.getElementById('root'));
