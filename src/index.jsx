ab4b7257f3ec142caf9a1caccb2eb5a56346f74a
DmitryScaletta <dmitryscaletta@mail.ru>
2019-05-03 16:07:14 +0300

    feat: initialize react-ga

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 
 import App from './App';
 import fullscreenPolyfills from './utils/fullscreen';
 
+ReactGA.initialize('UA-139550930-1');
 fullscreenPolyfills();
 
 ReactDOM.render(<App />, document.getElementById('root'));
