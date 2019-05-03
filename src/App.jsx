8d8e81fd5e80b97ff279fbfdb8748626be7c3066
DmitryScaletta <dmitryscaletta@mail.ru>
2019-05-03 13:01:49 +0300

    fix(App): url params for the player and chats

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useRef, useLayoutEffect } from 'react';
+import React, { useState } from 'react';
 import styled, { createGlobalStyle, css } from 'styled-components';
 
-import { getChatUrl, getPlayerUrl } from './utils/streamServices';
+import Player from './Player';
+import { STREAM_SERVICES, getChatUrl, getPlayerUrl } from './utils/streamServices';
+import { getPlayerFromUrl, getChatsFromUrl } from './utils/urlParams';
 
-document.title = 'HoneyNyticK - Twitch';
+document.title = 'HoneyMad';
 
 const GlobalStyle = createGlobalStyle`
   :root {
@@ -11,6 +13,7 @@ const GlobalStyle = createGlobalStyle`
     --color-violet: #2c2541;
     --color-violet-dark: #1f1925;
     --chat-tabs-height: 26px;
+    --chat-witdh: 340px;
   }
   body {
     margin: 0;
@@ -20,44 +23,37 @@ const Container = styled.div`
   display: flex;
   height: 100vh;
 
-  ${p => p.service === 'nonolive'
+  ${p => p.service === STREAM_SERVICES.NONOLIVE
     && css`
       &:after {
         content: '';
         position: absolute;
         left: 0;
-        right: 340px;
+        right: var(--chat-witdh);
         bottom: 0;
         height: 60px;
         background-color: #313131;
       }
     `};
 `;
-const Player = styled.iframe`
+const StyledPlayer = styled(Player)`
   position: relative;
   border: none;
   width: 100%;
+  margin-right: var(--chat-witdh);
 
-  ${p => p.service === 'nonolive'
+  ${p => p.service === STREAM_SERVICES.NONOLIVE
     && css`
-      margin-right: -340px;
+      margin-right: 0;
     `};
-`;
-const ChatWrapper = styled.div`
-  position: relative;
-  z-index: 1;
-  display: flex;
-  flex-direction: column;
-  flex-shrink: 0;
-  width: 340px;
 `;
 const ChatTabs = styled.div`
   position: absolute;
   top: 0;
-  left: 0;
   right: 0;
   display: flex;
   height: var(--chat-tabs-height);
+  width: var(--chat-witdh);
   border-bottom: 1px solid var(--color-violet);
 `;
 const ChatTab = styled.div`
@@ -78,66 +74,47 @@ const ChatTab = styled.div`
     border-right: 1px solid var(--color-violet);
   }
 `;
-const Chats = styled.div`
-  position: relative;
-  margin-top: var(--chat-tabs-height);
-  height: calc(100% - var(--chat-tabs-height));
-`;
 const Chat = styled.iframe`
   position: absolute;
-  top: 0;
-  left: 0;
+  top: var(--chat-tabs-height);
   right: 0;
-  bottom: 0;
   display: ${p => (p.active ? 'block' : 'none')};
-  z-index: ${p => (p.active ? 1 : 0)};
-  width: 100%;
-  height: 100%;
-  flex-grow: 1;
-  flex-shrink: 0;
+  width: var(--chat-witdh);
+  height: calc(100% - var(--chat-tabs-height));
   border: none;
   background-color: #fff;
 `;
 
-const player = { service: 'twitch', payload: 'honeymad' };
-
-const chats = [
-  { id: 1, service: 'twitch', payload: 'honeymad' },
-  { id: 2, service: 'twitch', payload: 'nytick' },
-];
-
 const App = () => {
-  const [activeChatId, setActiveChatId] = useState(chats[0].id);
-  const playerRef = useRef(null);
+  const urlParams = new URLSearchParams(window.location.search);
+  const player = getPlayerFromUrl(urlParams.get('player'));
+  const chats = getChatsFromUrl(urlParams.get('chats'));
 
-  useLayoutEffect(() => {
-    if (playerRef.current && player.service === 'twitch') {
-      playerRef.current.setAttribute('allowfullscreen', 'true');
-    }
-  }, []);
+  const [activeChatId, setActiveChatId] = useState(chats[0].id);
+
+  // Убираем чат nonolive если он совпадает с каналом плеера
+  const renderedChats = chats.filter(
+    chat => !(chat.service === STREAM_SERVICES.NONOLIVE && chat.payload === player.payload),
+  );
 
   return (
     <>
       <Container service={player.service}>
-        <Player name="player" src={getPlayerUrl(player)} service={player.service} ref={playerRef} />
-        <ChatWrapper>
-          <ChatTabs>
-            {chats.map(chat => (
-              <ChatTab
-                key={chat.id}
-                active={chat.id === activeChatId}
-                onClick={() => setActiveChatId(chat.id)}
-              >
-                {chat.payload}
-              </ChatTab>
-            ))}
-          </ChatTabs>
-          <Chats>
-            {chats.map(chat => (
-              <Chat key={chat.id} active={chat.id === activeChatId} src={getChatUrl(chat)} />
-            ))}
-          </Chats>
-        </ChatWrapper>
+        <StyledPlayer src={getPlayerUrl(player)} service={player.service} />
+        <ChatTabs>
+          {chats.map(chat => (
+            <ChatTab
+              key={chat.id}
+              active={chat.id === activeChatId}
+              onClick={() => setActiveChatId(chat.id)}
+            >
+              {chat.payload}
+            </ChatTab>
+          ))}
+        </ChatTabs>
+        {renderedChats.map(chat => (
+          <Chat key={chat.id} active={chat.id === activeChatId} src={getChatUrl(chat)} />
+        ))}
       </Container>
       <GlobalStyle />
     </>
