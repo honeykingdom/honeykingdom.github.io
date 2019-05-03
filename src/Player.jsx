b9313ef45a9c073289d93d2620c569e3b471de52
DmitryScaletta <dmitryscaletta@mail.ru>
2019-05-03 13:00:36 +0300

    feat(Player): add component

---
+++ src/Player.jsx
@@ -0,0 +1,25 @@
+import React, { useLayoutEffect, useRef } from 'react';
+import pt from 'prop-types';
+
+import { STREAM_SERVICES } from './utils/streamServices';
+
+const Player = ({ className, src, service }) => {
+  const playerRef = useRef(null);
+
+  useLayoutEffect(() => {
+    if (playerRef.current && service === STREAM_SERVICES.TWITCH) {
+      playerRef.current.setAttribute('allowfullscreen', 'true');
+    }
+  }, [service]);
+
+  // eslint-disable-next-line jsx-a11y/iframe-has-title
+  return <iframe className={className} src={src} service={service} ref={playerRef} />;
+};
+
+Player.propTypes = {
+  className: pt.string,
+  src: pt.string.isRequired,
+  service: pt.string.isRequired,
+};
+
+export default Player;
