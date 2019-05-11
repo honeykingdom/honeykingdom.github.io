48eba9546aa9966b1de4005a00fcfcb616606388
DmitryScaletta <dmitryscaletta@mail.ru>
2019-05-11 03:43:45 +0300

    feat: add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+export const STREAM_SERVICES = {
+  TWITCH: 'twitch',
+  GOODGAME: 'goodgame',
+  NONOLIVE: 'nonolive',
+  WASD: 'wasd',
+  // YOUTUBE: 'youtube',
+};
+
+export const PRESETS = {
+  nytick: {
+    player: 'nytick',
+    chats: 'honeymad,nytick',
+  },
+  gg: {
+    player: 'goodgame@41677',
+    chats: 'honeymad,goodgame@41677',
+  },
+  nonolive: {
+    player: 'nonolive@18407291',
+    chats: 'honeymad,nonolive@18407291',
+  },
+  wasd: {
+    player: 'wasd@22196',
+    chats: 'honeymad,wasd@22196',
+  },
+};
