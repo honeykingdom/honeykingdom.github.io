629e4f333eff43b2590b88de3613cb5109893a3b
DmitryScaletta <dmitryscaletta@mail.ru>
2019-05-13 20:51:45 +0300

    fix: update ga pageview

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,8 @@ import App from './App';
 import fullscreenPolyfills from './utils/fullscreen';
 
 ReactGA.initialize('UA-139550930-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
+ReactGA.pageview(window.location.pathname + window.location.search + window.location.hash);
+
 fullscreenPolyfills();
 
 ReactDOM.render(<App />, document.getElementById('root'));
