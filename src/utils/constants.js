95c91d548e5962e98c8adccb4b15f9af8deafa49
DmitryScaletta <dmitryscaletta@mail.ru>
2019-05-18 01:43:05 +0300

    feat: add new goodgame presets

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,6 +15,34 @@ export const PRESETS = {
     player: 'goodgame@41677',
     chats: 'honeymad,goodgame@41677',
   },
+  'gg-lasqa': {
+    player: 'goodgame@99600',
+    chats: 'lasqa,goodgame@99600',
+  },
+  'gg-segall': {
+    player: 'goodgame@99600',
+    chats: 'segall,goodgame@99600',
+  },
+  'gg-arrowwoods': {
+    player: 'goodgame@137757',
+    chats: 'arrowwoods,goodgame@137757',
+  },
+  'gg-melharucos': {
+    player: 'goodgame@25432',
+    chats: 'melharucos,goodgame@25432',
+  },
+  'gg-liz0n': {
+    player: 'goodgame@79564',
+    chats: 'liz0n,goodgame@79564',
+  },
+  'gg-roadhouse': {
+    player: 'goodgame@130743',
+    chats: 'roadhouse,goodgame@130743',
+  },
+  'gg-zanuda': {
+    player: 'goodgame@88446',
+    chats: 'zanuda,goodgame@88446',
+  },
   nonolive: {
     player: 'nonolive@18407291',
     chats: 'honeymad,nonolive@18407291',
