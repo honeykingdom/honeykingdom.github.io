56abe78126954d68dacea765fbce57108f3bc086
DmitryScaletta <dmitryscaletta@mail.ru>
2019-05-18 04:06:38 +0300

    fix: remove service property from player

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import pt from 'prop-types';
 
-import { STREAM_SERVICES } from './utils/constants';
-
 const Player = ({ className, src }) => (
   // eslint-disable-next-line jsx-a11y/iframe-has-title
   <iframe className={className} src={src} allow="autoplay" allowFullScreen="true" />
@@ -10,8 +8,6 @@ const Player = ({ className, src }) => (
 Player.propTypes = {
   className: pt.string,
   src: pt.string.isRequired,
-  // eslint-disable-next-line react/no-unused-prop-types
-  service: pt.oneOf(Object.values(STREAM_SERVICES)).isRequired,
 };
 
 export default Player;
