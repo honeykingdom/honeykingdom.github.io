5d2afd12a6f71c12d2d891d0c29c01519cc27061
DmitryScaletta <dmitryscaletta@mail.ru>
2019-05-18 04:06:18 +0300

    fix: remove fullscreen polyfils

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
 
 import App from './App';
-import fullscreenPolyfills from './utils/fullscreen';
 
 ReactGA.initialize('UA-139550930-1');
 ReactGA.pageview(window.location.pathname + window.location.search + window.location.hash);
-
-fullscreenPolyfills();
 
 ReactDOM.render(<App />, document.getElementById('root'));
