5834c857b979a63e47dd23b7ea4dc568fa4c7e95
DmitryScaletta <dmitryscaletta@mail.ru>
2020-01-19 08:30:02 +0300

    feat: rewrite with typescript

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
+
+import App from "./App";
+
+ReactGA.initialize("UA-139550930-1");
+ReactGA.pageview(
+  window.location.pathname + window.location.search + window.location.hash
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
